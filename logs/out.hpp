50d76129c9af4ac81902917d2531a197e5badfcd
Fengjinlong1210 <239383772@qq.com>
2023-08-21 21:28:58 +0800

    输出器

--- logs/out.hpp
+++ logs/out.hpp
@@ -8,6 +8,7 @@
 
 namespace Log
 {
+    // 基类输出, 提供一个log接口用来进行数据向指定方向的输出
     class Output
     {
     public:
@@ -16,7 +17,7 @@ namespace Log
         virtual ~Output() {}
         virtual void log(const char *data, size_t len) = 0;
     };
-
+    // 标准输出
     class StdOutput : public Output
     {
     public:
@@ -27,7 +28,7 @@ namespace Log
             std::cout.write(data, len);
         }
     };
-
+    // 向文件流中输出
     class FileOutput : public Output
     {
     public:
@@ -54,7 +55,7 @@ namespace Log
         std::string _pathname;
         std::ofstream _ofs;
     };
-
+    // 滚动文件输出, 根据文件大小进行滚动
     class RollOutput : public Output
     {
     public:
@@ -70,7 +71,7 @@ namespace Log
         {
             if (!_ofs.is_open() || _cur_size > _max_size)
             {
-                // 当前没有文件打开或大小超出限制
+                // 当前没有文件打开或大小超出限制时, 打开/切换文件
                 _ofs.close();
                 std::string name = createNewFileName();
                 _ofs.open(name, std::ios::binary | std::ios::app);
@@ -105,13 +106,13 @@ namespace Log
         }
 
     private:
-        size_t cnt = 0;
+        size_t cnt = 0; // 防止在一秒内创建了相同的文件
         std::string _basename;
         std::ofstream _ofs;
-        size_t _cur_size;
-        size_t _max_size;
+        size_t _cur_size; // 当前文件大小
+        size_t _max_size; // 文件最大限制
     };
-
+    // 根据时间进行滚动文件
     enum TimeGap
     {
         Sec,
@@ -119,7 +120,6 @@ namespace Log
         Hour,
         Day
     };
-
     class TimeRollOutput : public Output
     {
     public:
@@ -142,11 +142,12 @@ namespace Log
                 _gap_size = 3600 * 24;
                 break;
             }
+            // 初始化当前所处的时间段, 如果时间间隔为1, 则每秒都是一个时间段
             _cur_gap = _gap_size == 1 ? Util::Date::now() : Util::Date::now() / _gap_size;
             std::string filename = createNewFileName();
             // 如果路径不存在就创建路径, 然后打开文件
             Util::File::create_directory(Util::File::path(filename));
-            _ofs.open(filename, ios::app);
+            _ofs.open(filename, std::ios::app);
             assert(_ofs.is_open());
         }
 
@@ -154,17 +155,13 @@ namespace Log
         {
             // 当前系统时间 / 时间段大小  如果不是当前的时间段, 代表超过了规定的时间, 需要更换文件
             time_t cur = Util::Date::now();
-            if ((cur / _gap_size) != _cur_gap)
+            if ((cur / _gap_size) != _cur_gap) // 计算最新的时间段, 如果超过了上次的时间就切换文件
             {
-                // cout << "(cur / _gap_size) != _cur_gap = " << ((cur / _gap_size) != _cur_gap) << endl;
-                // cout << "cur time = " << cur << " cur gap = " << _cur_gap << endl;
-                // cout << "cur time / cur gap = " << (cur / _gap_size) << endl;
-                // cout << "-----------"<<endl;
                 _ofs.close();
                 std::string filename = createNewFileName();
-                _ofs.open(filename, ios::app);
+                _ofs.open(filename, std::ios::app);
+                // 更改当前文件的时间段
                 _cur_gap = _gap_size == 1 ? Util::Date::now() : Util::Date::now() / _gap_size;
-
                 assert(_ofs.is_open());
             }
             _ofs.write(data, len);
@@ -195,10 +192,11 @@ namespace Log
     private:
         std::string _basename;
         std::ofstream _ofs;
-        time_t _cur_gap;
-        time_t _gap_size;
+        time_t _cur_gap;  // 当前所处的时间段
+        time_t _gap_size; // 时间段的大小
     };
 
+    // 创建输出流的工厂, 返回输出类型的智能指针
     class OutputFactory
     {
     public:
