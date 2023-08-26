83c8211b6604fc9a370bea44ca0c4c07ac26d442
Fengjinlong1210 <239383772@qq.com>
2023-08-26 22:25:53 +0800

    异步工作器

--- logs/async.hpp
+++ logs/async.hpp
@@ -25,9 +25,11 @@ namespace Log
               _callback(cb),
               _async_type(async_type)
         {
+            //std::cout << "AsyncLooper construction"<< std::endl;
         }
         ~AsyncLooper()
         {
+            //std::cout << "AsyncLooper destruction"<< std::endl;
             stop();
         }
         void stop()
@@ -39,13 +41,16 @@ namespace Log
 
         void push(const char *data, size_t len)
         {
+            // std::cout << "push :: data = " << data << std::endl;
+            // std::cout << "push :: len = " << len << std::endl;
+
             // 设置临界资源的访问
             std::unique_lock<std::mutex> lock(_mutex);
             // 判断缓冲区是否满足条件(不扩容模式下才需要判断)
             if (_async_type == AsyncType::ASYNC_SAFE)
                 _cond_producer.wait(lock, [&]
-                                    { return _buff_producer.readableSize() >= len; });
-            // 向缓冲区压入数据
+                                    { return _buff_producer.writeableSize() >= len; });
+            // 向生产缓冲区压入数据
             _buff_producer.push(data, len);
             // 唤醒消费者线程对缓冲区数据进行处理
             _cond_consumer.notify_one();
@@ -59,23 +64,31 @@ namespace Log
                 // 设置一段临界区, 只对缓冲区的交换进行上锁, 不对数据处理上锁
                 {
                     std::unique_lock<std::mutex> lock(_mutex);
-                    // 如果退出状态为真, 或者生产缓冲区不为空时, 唤醒消费者线程, 否则继续休眠
+                    // 当退出状态为真且生产缓冲区为空时退出
                     if (_stop && _buff_producer.empty())
+                    {
+                        //std::cout << _buff_consumer.getReadableData();
+                        //std::cout << _buff_producer.getReadableData();
+                        //std::cout << "async looper exit" << std::endl;
                         break;
-
+                    }
+                    // 如果退出状态为真, 或者生产缓冲区不为空时, 唤醒消费者线程, 否则继续休眠
                     _cond_consumer.wait(lock, [&]
                                         { return !_stop || !_buff_producer.empty(); });
                     // 生产缓冲区有数据, 交换两个缓冲区
-                    _buff_consumer.swap(_buff_producer);
+                    //_buff_consumer.swap(_buff_producer);
+                    _buff_producer.swap(_buff_consumer);
                     // 唤醒生产者
-                    if (_async_type == AsyncType::ASYNC_SAFE)
-                        _cond_producer.notify_all();
                 }
+                if (_async_type == AsyncType::ASYNC_SAFE)
+                    _cond_producer.notify_all();
                 // 被唤醒后, 处理消费缓冲区中的数据
                 _callback(_buff_consumer);
-                // 重制缓冲区
+                // std::cout << "readable size = " << _buff_consumer.readableSize() << std::endl;
+                //  重制缓冲区
                 _buff_consumer.reset();
             }
+            //std::cout << "while exit" << std::endl;
         }
 
     private:
